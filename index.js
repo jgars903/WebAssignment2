6d4f0f7cdb3e757773a974ab5b4d1b17cea4fb94
jgars903 <jgars903@mtroyal.ca>
2022-12-07 22:15:49 -0700

    Added comments and referenes

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+// Chart JS documentation
+// https://www.chartjs.org/docs/latest/
+// https://www.chartjs.org/docs/latest/charts/radar.html
+
+// URL and some variables to hold the API data
 const url = "http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php";
 var data;
 var playlist = [];
 
+// Disable the Artist and Genre and grayed it out.
 document.getElementById('artist').disabled = true;
 document.getElementById('genre').disabled = true;
 document.getElementById('f2').style.color = "grey";
 document.getElementById('f3').style.color = "grey";
 
+// Disable search fields based on the user selection of the radio buttons
 function disable() {
     if (document.getElementById("t").checked === true) {
         document.getElementById('title').disabled = false;
@@ -32,7 +39,9 @@ function disable() {
     }
 }
 
+// Call the API to get the song details
 async function getapi(api_url) {
+    // If there is no data then call the API
     if (JSON.parse(localStorage.getItem('songs') === null)) {
         const response = await fetch(url);
         data = await response.json();
@@ -42,28 +51,35 @@ async function getapi(api_url) {
         localStorage.setItem('songs', JSON.stringify(data));
         console.log("Fetched data");
     } else {
+        // If there is song data in the local storage then grab it from the local storage
         data = JSON.parse(localStorage.getItem('songs'));
         console.log("Local Storage data");
         loader();
     } sortTable('tit');
 }
 
+// Call the getapi function
 getapi(url);
 
+// Display the loader
 function loader() {
     document.getElementById('loading').style.display = 'none';
 }
 
+// Set the radar chart display as non or flex
 function radarChart(shown, hidden) {
     document.getElementById(shown).style.display = 'flex';
     document.getElementById(hidden).style.display = 'none';
     return false;
 }
 
-
+// Radar chart specifications
 function page(r, artist, genre, year, duration, bpm, popularity, energy, danceability, liveness, valence, acousticness, speechiness, loudness) {
+    // Minutes and seconds
     var minutes = Math.floor(duration / 60);
     var seconds = duration - minutes * 60;
+
+    // Set the data for the 'info' tag as below
     document.getElementById('info').innerHTML = `${r}, ${artist}, ${genre}, ${year}, ${minutes}:${seconds}<br/><h3 class="data" id="analysis">Data Analytics:</h3> <div class="data" style="list-style-type:disc">Bpm: ${bpm}<br/>
       Energy: ${energy}<br/>
       Dancebility: ${danceability}<br/>
@@ -74,6 +90,7 @@ function page(r, artist, genre, year, duration, bpm, popularity, energy, danceab
       Popularity: ${popularity}</div>`;
     var marksCanvas = document.getElementById("radarChart");
 
+    // Factors and dataset for the radar chart
     var marksData = {
         labels: [
             "danceability",
@@ -99,11 +116,13 @@ function page(r, artist, genre, year, duration, bpm, popularity, energy, danceab
         ]
     };
 
+    // Create the chart
     var radarChart = new Chart(marksCanvas, {
         type: 'radar',
         data: marksData
     });
 }
+
 
 function frequent(arr1) {
     var mf = 1,
@@ -124,6 +143,7 @@ function frequent(arr1) {
     return item;
 }
 
+// Display the radar chart with all the information
 function show(data, id) {
     let rec = '',
         avg = 0,
@@ -238,6 +258,7 @@ function show(data, id) {
     </tr>`;
         }
     }
+
     document.getElementById(id).innerHTML = rec;
     avg = Math.round(avg / playlist.length) ? Math.round(avg / playlist.length) : '-';
     max_year = yea_n.length === 0 ? '-' : Math.max(... yea_n);
@@ -255,6 +276,7 @@ function show(data, id) {
                                                       </div>`
 }
 
+// Adding the event handlers for the column headings i-e title, artist, year, genre and popularity
 document.getElementById("tit").addEventListener("click", () => {
     sortTable('tit');
 });
@@ -271,6 +293,7 @@ document.getElementById("popularity").addEventListener("click", () => {
     sortTable('popularity');
 });
 
+// It sorts the song details
 function sortTable(getid) {
     const elements = Array.from(document.getElementsByClassName('thHead'));
     elements.forEach(element => {
@@ -279,6 +302,7 @@ function sortTable(getid) {
 
     document.getElementById(getid).style.color = '#8B0000';
     document.getElementById('loading').style.display = 'inline';
+    // SOrts based on title
     if (getid === 'tit') {
         data.sort(function (a, b) {
             if (typeof a.title != 'string') {
@@ -297,7 +321,7 @@ function sortTable(getid) {
             }
             return 0;
         });
-    } else if (getid === 'art') {
+    } else if (getid === 'art') { // Sort based on the artist
         data.sort(function (a, b) {
             if (typeof a.artist.name != 'string') {
                 a.artist.name = a.artist.name.toString();
@@ -315,7 +339,7 @@ function sortTable(getid) {
             }
             return 0;
         });
-    } else if (getid === 'year') {
+    } else if (getid === 'year') { // Sort based on the year
         data.sort(function (a, b) {
             if (typeof a.year != 'string') {
                 a.year = a.year.toString();
@@ -333,7 +357,7 @@ function sortTable(getid) {
             }
             return 0;
         });
-    } else if (getid === 'gen') {
+    } else if (getid === 'gen') { // Sort based on the genre
         data.sort(function (a, b) {
             if (typeof a.genre.name != 'string') {
                 a.genre.name = a.genre.name.toString();
@@ -351,7 +375,7 @@ function sortTable(getid) {
             }
             return 0;
         });
-    } else if (getid === 'popularity') {
+    } else if (getid === 'popularity') { // Sort based on the popularity
         data.sort(function (a, b) {
             if (typeof a.details.popularity != 'string') {
                 a.details.popularity = a.details.popularity.toString();
@@ -374,7 +398,7 @@ function sortTable(getid) {
     show(data, 'ProductSpace');
 }
 
-
+// It shows an alert of info window saying the song has been added to the playlist
 function snackbar(song_id) {
     var x = document.getElementById("snackbar");
     x.className = "show";
@@ -384,6 +408,7 @@ function snackbar(song_id) {
     playlist.push(song_id);
 }
 
+// It shows the credits section i-e Owner of the code nd GitHub link
 function snackbar_org() {
     var x = document.getElementById("credits");
     x.className = "show";
@@ -392,12 +417,14 @@ function snackbar_org() {
     }, 5000);
 }
 
+// It filters based on the type of radio button selected
 function songFilter() {
     let fil = data.filter(check);
     document.getElementById("ProductSpace").innerHTML = '';
     show(fil, 'ProductSpace');
 }
 
+// It checks which radio button is clicks out of title, genre and year
 function check(r) {
     var filter = document.getElementsByName('opt');
     for (i = 0; i < filter.length; i ++) {
@@ -414,27 +441,27 @@ function check(r) {
             } else {
                 if (r.artist.name.includes(document.getElementById(filter[i].value).value)) 
                     return r.song_id;
-                
-
             }
         }
     }
 }
 
+// Remove the song from playlist
 function remove(id) {
     for (let i = 0; i < playlist.length; i++) {
         if (playlist[i] == id) 
             playlist.splice(i, 1);
-        
     }
     show(data, 'play');
 }
 
+// Clear all the songs from playlist 
 function clear1() {
     playlist = [];
     show(data, 'play');
 }
 
+// Remove all the filters
 function removeFilter() {
     sortTable('tit');
     document.getElementById('title').value = '';
